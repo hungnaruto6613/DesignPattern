9e378037f4aad75b6b950bd0b8392a964e2a3fa7
Hung Khung <Hung Khung@DESKTOP-78KNM87>
2022-01-15 11:02:09 +0700

    Add project files.

---
+++ BuilderPattern/Builder/HtmlElement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern.Builder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        public const int indentSize = 2;
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+        }
+
+        public HtmlElement()
+        {
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.AppendLine($"{ i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var element in Elements)
+            {
+                sb.Append(element.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement
+            {
+                Name = rootName,
+            };
+        }
+    }
+}
---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using BuilderPattern.Builder;
+using System;
+
+namespace BuilderPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Builder
+            var bd = new HtmlBuilder("ul");
+            bd.AddChild("li", "Hello").AddChild("li", "Goodbye");
+            Console.WriteLine(bd.ToString());
+            Console.ReadLine();
+        }
+    }
+}
